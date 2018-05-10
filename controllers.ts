b9de7c27ea7f41b1a1522d48c403b1f974b08d10
rksquared <rahul@rahulkrish.com>
2018-05-10 10:37:14 -0400

    fixed merge conflict with wikipaths in ctrl

--- controllers.ts
+++ controllers.ts
@@ -1,6 +1,12 @@
 import { Router, Request, Response } from 'express';
+<<<<<<< HEAD
 import { getShortestPath, getAllPaths, getSurroundings, getNodesList } from './database/neo4j/wikipediaPaths';
 import { getTweetsByUser } from './twitterAPI/twitterCallers';
+=======
+import { getShortestPath, getAllPaths, getSurroundings, getNodesList } from './database/wikipediaPaths';
+import * as fs from 'fs';
+import * as path from 'path';
+>>>>>>> merge conflicts for controller imports managed
 
 const router: Router = Router();
 
@@ -31,7 +37,21 @@ router.post('/api/surroundings', async (req: Request, res: Response) => {
 router.get('/api/nodesList', (req: Request, res: Response) => {
   const result = getNodesList();
   res.send(result);
-})
+});
+
+router.get('/api/embedding', (req: Request, res: Response) => {
+  const sampleData: number[] = [];
+
+  fs.readFile(path.join(__dirname + './sampleData/embedding.csv'), 'utf8', (err, data) => {
+    if (err) { return console.error('error extracting sample data from csv', err); }
+
+    data.split(',').forEach((coord) => {
+      sampleData.push(parseFloat(coord));
+    });
+
+    res.send(sampleData);
+  });
+});
 
 export {
   router
