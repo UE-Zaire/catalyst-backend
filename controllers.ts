0da17bd08f79a531bf71059146b4bc3c9751bd54
Pedro Barquinha <barquinha.pedro@gmail.com>
2018-05-11 14:22:09 -0400

    Added groups to coordinates of cluster

--- controllers.ts
+++ controllers.ts
@@ -2,17 +2,33 @@ import { Router, Request, Response } from 'express';
 import { getShortestPath, getAllPaths, getSurroundings, getNodesList } from './database/neo4j/wikipediaPaths';
 import { getTweets } from './database/mySQL/twitterStorage';
 import { getUserFollowers } from './twitterAPI/twitterCallers';
+import passport from 'passport';
 import * as fs from 'fs';
 import * as path from 'path';
 
 const router: Router = Router();
 
+//---------------------------------------------------------- LOGIN
+
+router.get('/twitter/login',
+  passport.authenticate('twitter'));
+
+router.get('/logged', 
+  passport.authenticate('twitter', { failureRedirect: '/api/surroundings', successRedirect: '/api/surroundings' }),
+  function(req, res) {
+    console.log('auth logged')
+    res.redirect('/');
+  });
+
+//---------------------------------------------------------- TWITTER 
+
 router.post('/api/twitterFollowers', async (req, res) => {
   const { username } = req.body;
   const results = await getUserFollowers(username);
-  //console.log(results);
   res.send(results);
 });
+
+//---------------------------------------------------------- WIKIPEDIA
 
 router.post('/api/path', async (req: Request, res: Response) => {
   const { source, target } = req.body;
@@ -37,9 +53,11 @@ router.get('/api/nodesList', (req: Request, res: Response) => {
   res.send(result);
 });
 
+//----------------------------------------------------------- SAMPLE DATA
+
 router.get('/api/embedding', (req: Request, res: Response) => {
   const sampleData: object[] = [];
-
+  console.log('embed')
   fs.readFile(path.join(__dirname + '/sampleData/embedding.csv'), 'utf8', (err, data) => {
     if (err) { return console.error('error extracting sample data from csv', err); }
 
@@ -49,7 +67,13 @@ router.get('/api/embedding', (req: Request, res: Response) => {
       sampleData.push([parseFloat(csvData[i]), parseFloat(csvData[i + 1])]);
     }
 
-    res.send(sampleData);
+    fs.readFile(path.join(__dirname + '/sampleData/clusters10.csv'), 'utf8', (err, data) => {
+      if (!err) {
+        const clusterData = data.split('\n');
+        const comb = sampleData.map((arr: any, i: number) => [...arr, parseInt(clusterData[i])]);
+        res.send(comb);
+      } 
+    })
   });
 });
 
--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 import express from 'express';
 import { router } from './controllers';
 import bodyParser from 'body-parser'; 
+import passport from 'passport';
+import { Strategy } from 'passport-twitter';
+import session from 'express-session';
+import keys from './config';
+
 
 const app: express.Application = express();
 const port: number = (process.env.PORT !== undefined) ? parseInt(process.env.PORT): 3005;
@@ -11,6 +16,19 @@ app.use((req, res, next) => {
     res.header(`Access-Control-Allow-Headers`, `Origin, X-Requested-With, Content-Type, Accept`);
     next();
 });
+passport.use(new Strategy({
+  consumerKey: keys.consumer_key,
+  consumerSecret: keys.consumer_secret,
+  callbackURL: "http://localhost/3005/logged"
+}, (token, tokenSecret, profile, done) => {
+  console.log('logged', token);
+}))
+app.use(session({ secret: 'cats', resave: true, saveUninitialized: true }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.use('/', router);
 
 app.listen(port, () => {
